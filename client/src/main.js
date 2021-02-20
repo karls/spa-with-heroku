7c67e3cc1fec0c808d4b84947912a67419862e24
Karl Sutt <karl@sutt.ee>
2021-02-20 21:24:06 +0200

    Initial commit

---
+++ client/src/main.js
@@ -0,0 +1,7 @@
+import maincss from "./main.css";
+
+// __ENV__ is getting replaced by the replace plugin for Rollup.
+// Check client/rollup.config.js
+//
+console.log("Running in __ENV__.");
+console.log("Nothing changes between development and production, except running build with NODE_ENV=production.");
